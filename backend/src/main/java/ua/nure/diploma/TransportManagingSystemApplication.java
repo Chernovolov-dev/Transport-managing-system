6024483d0b033488732e333cddb7260d71d51772
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-05-10 23:55:04 +0300

    Initial commit in backend branch

---
+++ backend/src/main/java/ua/nure/diploma/TransportManagingSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagingSystemApplication.class, args);
+	}
+
+}
