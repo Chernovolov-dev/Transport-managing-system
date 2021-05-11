7f51c7e990f5f6e6fe7ed3a074f5d5110520a8cd
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-05-11 18:54:27 +0300

    Added configure method in SecurityConfig.java

--- backend/src/main/java/ua/nure/diploma/configs/SecurityConfig.java
+++ backend/src/main/java/ua/nure/diploma/configs/SecurityConfig.java
@@ -1,4 +1,21 @@
 package ua.nure.diploma.configs;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login/**").permitAll()
+                .antMatchers("/request/**").hasAuthority("ROLE_REQUEST_MANAGER")
+                .antMatchers("/routeSheet/**").hasAuthority("ROLE_LOGIST")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutSuccessUrl("/login/**");
+    }
 }
