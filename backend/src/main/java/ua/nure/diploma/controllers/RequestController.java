7b03b12e6f787651b53adf7eeb214e70a98074cb
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-05-11 20:19:05 +0300

    Added first method in RequestController.java

--- backend/src/main/java/ua/nure/diploma/controllers/RequestController.java
+++ backend/src/main/java/ua/nure/diploma/controllers/RequestController.java
@@ -1,4 +1,17 @@
 package ua.nure.diploma.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class RequestController {
+
+    @GetMapping("request/all")
+    public String getAllRequests(){
+        return "You are on a manager request page";
+    }
+
 }
