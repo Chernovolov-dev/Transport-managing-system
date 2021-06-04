da98a55e63d28435c8fbd3d28dc5848d4102e1da
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-04 23:47:20 +0300

    Added DaoAuthenticationProvider and passwordEncoder methods

--- backend/src/main/java/ua/nure/diploma/configs/SecurityConfig.java
+++ backend/src/main/java/ua/nure/diploma/configs/SecurityConfig.java
@@ -1,8 +1,12 @@
 package ua.nure.diploma.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,5 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .logout().logoutSuccessUrl("/login/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        authenticationProvider.setUserDetailsService();
+
+        return authenticationProvider;
     }
 }
