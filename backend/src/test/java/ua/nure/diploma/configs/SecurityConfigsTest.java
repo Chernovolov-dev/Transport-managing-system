239f157c184466d426bc5cbee9d71d1ee0247d59
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-04 20:39:03 +0300

    Added method to check redirection when calling the /deliveryRequest endpoint without authorization

---
+++ backend/src/test/java/ua/nure/diploma/configs/SecurityConfigsTest.java
@@ -0,0 +1,27 @@
+package ua.nure.diploma.configs;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityConfigsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("When calling the /deliveryRequest endpoint without authorization we expect to get a 301 Redirection.")
+    void testUnauthorizedDeliveryRequestEndpoint() throws Exception{
+
+        mockMvc.perform(get("/deliveryRequest"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
