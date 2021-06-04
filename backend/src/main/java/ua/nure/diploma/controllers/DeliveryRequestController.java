635580f4cde75df0f567ec91c7e71a2049d3691e
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-04 20:47:37 +0300

    Renamed "RequestController" to "DeliveryRequestController"

--- backend/src/main/java/ua/nure/diploma/controllers/DeliveryRequestController.java
+++ backend/src/main/java/ua/nure/diploma/controllers/DeliveryRequestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class RequestController {
+public class DeliveryRequestController {
 
     @GetMapping("deliveryRequest/all")
     public String getAllRequests(){
