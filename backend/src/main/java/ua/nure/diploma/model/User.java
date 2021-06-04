5d54a47f3184d6114a7174f9b41bd82a1eabb798
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-04 22:40:31 +0300

    Added model User.java and filled it with data

---
+++ backend/src/main/java/ua/nure/diploma/model/User.java
@@ -0,0 +1,55 @@
+package ua.nure.diploma.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class User {
+
+    private Long userId;
+    private String userName;
+    private String userPassword;
+    private String userAuthority;
+
+    public User(){}
+
+    public User(Long userId, String userName, String userPassword, String userAuthority) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userAuthority = userAuthority;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserAuthority() {
+        return userAuthority;
+    }
+
+    public void setUserAuthority(String userAuthority) {
+        this.userAuthority = userAuthority;
+    }
+}
