367d9a9e69f2d59cebeebe7c3fde3fe904574297
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-04 21:07:27 +0300

    Added RouteSheetController.java

--- backend/src/main/java/ua/nure/diploma/controllers/RouteSheetController.java
+++ backend/src/main/java/ua/nure/diploma/controllers/RouteSheetController.java
@@ -1,4 +1,16 @@
 package ua.nure.diploma.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/routeSheet")
 public class RouteSheetController {
+
+    @GetMapping("/manage")
+    public String getRouteSheetPanel(){
+
+        return "We are on the routeSheet panel page";
+    }
 }
