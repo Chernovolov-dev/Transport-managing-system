d59853975cd10786b7e86f60a92267db387f789d
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-05 22:42:44 +0300

    Added UserService.java to manage users themselves

---
+++ backend/src/main/java/ua/nure/diploma/services/UserService.java
@@ -0,0 +1,45 @@
+package ua.nure.diploma.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ua.nure.diploma.models.User;
+import ua.nure.diploma.repositories.UserRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user=userRepository.findByUserName(username);
+
+        if(user==null) throw new UsernameNotFoundException(String.format("User '%s' not found",username));
+
+        return new org.springframework.security.core.userdetails.User(user.getUserName(),
+                user.getUserPassword(),getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user){
+
+        List<String> authorities= Collections.singletonList(user.getUserAuthority());
+
+        return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+}
