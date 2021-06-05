dd340bcbfeac116e6d8e1f4609ec8aa32f40d7ec
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-05 23:17:56 +0300

    Deleted GeneratedValue annotation from User.java

--- backend/src/main/java/ua/nure/diploma/models/User.java
+++ backend/src/main/java/ua/nure/diploma/models/User.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String userName;
     private String userPassword;
