cdfe556c769ba7c8164243cb225016a518371378
Vladislav Chernovolov <chernovolov.work@gmail.com>
2021-06-08 16:36:55 +0300

    Added method to test authorization with wrong name

--- backend/src/test/java/ua/nure/diploma/configs/SecurityConfigsTest.java
+++ backend/src/test/java/ua/nure/diploma/configs/SecurityConfigsTest.java
@@ -1,22 +1,32 @@
 package ua.nure.diploma.configs;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.nure.diploma.repositories.UserRepository;
+import ua.nure.diploma.services.UserService;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SecurityConfigsTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @Test
     @DisplayName("When calling the /deliveryRequest endpoint without authorization we expect to get a 301 Redirection.")
@@ -64,9 +74,18 @@ class SecurityConfigsTest {
     @Test
     @DisplayName("When calling the deliveryRequest/all endpoint with role of the request manager")
     @WithMockUser(roles="REQUEST_MANAGER")
-    void testRequestManagerdeliveryRequestAllEndpoint() throws Exception{
+    void testRequestManagerDeliveryRequestAllEndpoint() throws Exception{
 
         mockMvc.perform(get("/deliveryRequest/all"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("When trying to login with wrong username we expect to get UsernameNotFoundException")
+    void testLoginWithWrongUsername(){
+
+        String actualName="Custom";
+
+        Assert.assertThrows(UsernameNotFoundException.class, ()-> userService.loadUserByUsername(actualName));
     }
 }
